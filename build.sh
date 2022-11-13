69c141264bcdf2322df8e8043f519c1a7e17da52
allandeng <allandeng97@foxmail.com>
2022-11-13 22:55:38 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 rm go-blog
+rm go-blog.tar
+rm -r ./tmp
 
 echo `go version`
-
 go build -o go-blog ./main.go
+
+mkdir tmp
+mkdir -p tmp/config
+cp -r ./static ./tmp
+cp -r ./views ./tmp
+cp ./config/*.yaml ./tmp/config
+cp ./go-blog ./tmp
+
+cd tmp
+tar -cvf go-blog.tar ./*
+cp go-blog.tar ..
